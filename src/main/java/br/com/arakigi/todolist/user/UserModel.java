c2f3cfde3bd019a2ac1622e9993c8d94c80b4058
Giovanni Sarao Araki <giovanni.saraoaraki@gmail.com>
2023-10-13 12:44:21 -0300

    todolist estructure

---
+++ src/main/java/br/com/arakigi/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.arakigi.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+  
+  private String username;
+  private String name;
+  private String password;
+
+ 
+}
