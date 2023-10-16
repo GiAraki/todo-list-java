4f63e22a9a05b73ffa8a75f32fd90e20991e3ef5
Giovanni Sarao Araki <giovanni.saraoaraki@gmail.com>
2023-10-15 21:14:06 -0300

    Import Entity

--- src/main/java/br/com/arakigi/todolist/user/UserController.java
+++ src/main/java/br/com/arakigi/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.arakigi.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel){
-    System.out.println(userModel.getName());
+  public void create(@RequestBody UserModel userModel) {
+    this.userRepository.save(userModel);
   }
 }
